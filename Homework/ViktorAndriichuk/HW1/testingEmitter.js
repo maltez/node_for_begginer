e1e7cb58ff04bef2fb4246c4d9c1ee2865bd15a6
avnicolaevich <avnicolaevich@gmail.com>
2018-09-07 02:02:01 +0300

    ready to verify homework 1

--- Homework/ViktorAndriichuk/HW1/testingEmitter.js
+++ Homework/ViktorAndriichuk/HW1/testingEmitter.js
@@ -1,26 +1,19 @@
 const { EventEmitter } = require('events');
-const amount = require("./testingFunction");
 const testing = new EventEmitter();
-const TEST = 'Testing amount function';
+const TEST = 'Testing the addition function for';
 
-testing.on(TEST, () => {
-		let result = amount(6, 7),
-			expected = 14;
-		if (result !== expected) {
-			console.log(`${TEST}: Test failed`);
-		} else {
-			console.log(`${TEST}: Test passed successfully`);
-		}
+testing.on(TEST, (a, b) => {
+	const result = a + b;
+	const expected = 6;
+	result !== expected ? console.error(`${TEST} equality ${expected}: Test failed`) : console.log(`${TEST} equality ${expected}: Test passed successfully`);
+		
 })
 
-testing.on(TEST, () => {
-	let result = amount(2, 2),
-		expected = 4;
-	if (result !== expected) {
-		console.log(`${TEST}: Test failed`);
-	} else {
-		console.log(`${TEST}: Test passed successfully`);
-	}
+testing.on(TEST, (a, b) => {
+	!a && !b ? console.error(`${TEST} arguments: Test failed`) : console.log(`${TEST} arguments: Test passed successfully`);
 })
 
+testing.emit(TEST, 6, 7);
+testing.emit(TEST, 7);
+testing.emit(TEST, 3, 3);
 testing.emit(TEST);
